d989611eb3b0cfaeb48122e699fdcb9b047aaad7
Danilo Kallan <danilokallan@hotmail.com>
2018-02-21 19:46:12 +0100

    Added Up untill InitializeSentence()

---
+++ Engine/Engine/textclass.cpp
@@ -0,0 +1,255 @@
+////////////////////////////////////////////////////////////////////////////////
+// filename: textclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "textclass.h"
+
+
+TextClass::TextClass()
+{
+	m_Font = 0;
+	m_FontShader = 0;
+
+	m_sentence1 = 0;
+	m_sentence2 = 0;
+}
+
+
+TextClass::TextClass(const TextClass& other)
+{
+}
+
+
+TextClass::~TextClass()
+{
+}
+
+
+bool TextClass::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, HWND hwnd, int screenWidth, int screenHeight,
+	XMMATRIX baseViewMatrix)
+{
+	bool result;
+
+
+	// Store the screen width and height.
+	m_screenWidth = screenWidth;
+	m_screenHeight = screenHeight;
+
+	// Store the base view matrix.
+	m_baseViewMatrix = baseViewMatrix;
+
+	// Create the font object.
+	m_Font = new FontClass;
+	if (!m_Font)
+	{
+		return false;
+	}
+
+	// Initialize the font object.
+	result = m_Font->Initialize(device, deviceContext, "../Engine/data/fontdata.txt", "../Engine/data/font.dds");
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the font object.", L"Error", MB_OK);
+		return false;
+	}
+
+	// Create the font shader object.
+	m_FontShader = new FontShaderClass;
+	if (!m_FontShader)
+	{
+		return false;
+	}
+
+	// Initialize the font shader object.
+	result = m_FontShader->Initialize(device, hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the font shader object.", L"Error", MB_OK);
+		return false;
+	}
+	
+	// Initialize the first sentence.
+	result = InitializeSentence(&m_sentence1, 16, device);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Now update the sentence vertex buffer with the new string information.
+	result = UpdateSentence(m_sentence1, "Hello", 100, 100, 1.0f, 1.0f, 1.0f, deviceContext);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Initialize the first sentence.
+	result = InitializeSentence(&m_sentence2, 16, device);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Now update the sentence vertex buffer with the new string information.
+	result = UpdateSentence(m_sentence2, "Goodbye", 100, 200, 1.0f, 1.0f, 0.0f, deviceContext);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+void TextClass::Shutdown()
+{
+	// Release the first sentence.
+	ReleaseSentence(&m_sentence1);
+
+	// Release the second sentence.
+	ReleaseSentence(&m_sentence2);
+
+	// Release the font shader object.
+	if (m_FontShader)
+	{
+		m_FontShader->Shutdown();
+		delete m_FontShader;
+		m_FontShader = 0;
+	}
+
+	// Release the font object.
+	if (m_Font)
+	{
+		m_Font->Shutdown();
+		delete m_Font;
+		m_Font = 0;
+	}
+
+	return;
+}
+
+
+bool TextClass::Render(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix, XMMATRIX orthoMatrix)
+{
+	bool result;
+
+
+	// Draw the first sentence.
+	result = RenderSentence(deviceContext, m_sentence1, worldMatrix, orthoMatrix);
+	if (!result)
+	{
+		return false;
+	}
+
+	// Draw the second sentence.
+	result = RenderSentence(deviceContext, m_sentence2, worldMatrix, orthoMatrix);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+bool TextClass::InitializeSentence(SentenceType** sentence, int maxLength, ID3D11Device* device)
+{
+	VertexType* vertices;
+	unsigned long* indices;
+	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+	int i;
+
+
+	// Create a new sentence object.
+	*sentence = new SentenceType;
+	if (!*sentence)
+	{
+		return false;
+	}
+
+	// Initialize the sentence buffers to null.
+	(*sentence)->vertexBuffer = 0;
+	(*sentence)->indexBuffer = 0;
+
+	// Set the maximum length of the sentence.
+	(*sentence)->maxLength = maxLength;
+
+	// Set the number of vertices in the vertex array.
+	(*sentence)->vertexCount = 6 * maxLength;
+
+	// Set the number of indexes in the index array.
+	(*sentence)->indexCount = (*sentence)->vertexCount;
+
+	// Create the vertex array.
+	vertices = new VertexType[(*sentence)->vertexCount];
+	if (!vertices)
+	{
+		return false;
+	}
+
+	// Create the index array.
+	indices = new unsigned long[(*sentence)->indexCount];
+	if (!indices)
+	{
+		return false;
+	}
+
+	// Initialize vertex array to zeros at first.
+	memset(vertices, 0, (sizeof(VertexType) * (*sentence)->vertexCount));
+
+	// Initialize the index array.
+	for (i = 0; i<(*sentence)->indexCount; i++)
+	{
+		indices[i] = i;
+	}
+
+	// Set up the description of the dynamic vertex buffer.
+	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * (*sentence)->vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	// Give the subresource structure a pointer to the vertex data.
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	// Create the vertex buffer.
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &(*sentence)->vertexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Set up the description of the static index buffer.
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * (*sentence)->indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	// Give the subresource structure a pointer to the index data.
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	// Create the index buffer.
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &(*sentence)->indexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// Release the vertex array as it is no longer needed.
+	delete[] vertices;
+	vertices = 0;
+
+	// Release the index array as it is no longer needed.
+	delete[] indices;
+	indices = 0;
+
+	return true;
+}
