abdb350978f5ff77ec71bec4156453abc6cca6cc
Danilo Kallan <danilokallan@hotmail.com>
2018-03-09 09:27:44 +0100

    Added Font.tga

--- Engine/Engine/textclass.cpp
+++ Engine/Engine/textclass.cpp
@@ -45,7 +45,7 @@ bool TextClass::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceCont
 	}
 
 	// Initialize the font object.
-	result = m_Font->Initialize(device, deviceContext, "../Engine/fontdata.txt", "../Engine/font.dds");
+	result = m_Font->Initialize(device, deviceContext, "../Engine/resources/font/fontdata.txt", "../Engine/resources/font/font.tga");
 	if (!result)
 	{
 		MessageBox(hwnd, L"Could not initialize the font object.", L"Error", MB_OK);
